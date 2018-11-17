68425798a71caeaf5daed2de9086fbb6955a5ae3
fedelaper <fedelaper@hotmail.com>
2018-11-17 11:20:24 -0300

    Primera aproximacion a la solucion

--- src/cmc/CmcDemoB.java
+++ src/cmc/CmcDemoB.java
@@ -1,4 +1,5 @@
 package cmc;
+
 import java.awt.Color;
 /**
  * Obtiene los dos primeros puntos marcados en la matriz (mapa)
@@ -18,49 +19,223 @@ import mapa.MapaInfo;
 public class CmcDemoB {
 	private MapaInfo mapa;
 	private CmcImple cmc;
-	
+
 	public CmcDemoB(MapaInfo mapa, CmcImple cmc) {
 		this.mapa = mapa;
 		this.cmc = cmc;
 		demoObtenerCamino();
 	}
-	
+
 	private void demoObtenerCamino() {
 		Punto a = mapa.getPuntos().get(0);
 		Punto b = mapa.getPuntos().get(1);
-		List<Punto> listaPuntos = expandirPuntosContiguos(a, b);
-		cmc.dibujarCamino(listaPuntos,Color.red);
+		List<PuntoPeso> visitados = new ArrayList<PuntoPeso>();
+		List<PuntoPeso> listaPuntosP = buscarCamino(puntoToPuntoPeso(a, mapa.getDensidad(a.x, a.y)),
+				puntoToPuntoPeso(b, mapa.getDensidad(b.x, b.y)), visitados);
+		List<Punto> listaPuntos = listaPPtoPunto(listaPuntosP);
+		cmc.dibujarCamino(listaPuntos, Color.red);
 		mapa.enviarMensaje("Camino minimo: " + listaPuntos.size() + " puntos");
-		
+
 	}
-	
-	private List<Punto> expandirPuntosContiguos(Punto a, Punto b) {
-		List<Punto> listaPuntos = new ArrayList<Punto>();
+
+	private List<PuntoPeso> buscarCamino(PuntoPeso a, PuntoPeso b, List<PuntoPeso> visitados) {
+		List<PuntoPeso> listaPuntos = new ArrayList<PuntoPeso>();
+		List<PuntoPeso> posiblesCaminos = buscarPuntos(a,b);
+
+		List<PuntoPeso> menor = new ArrayList<PuntoPeso>();
+		for (PuntoPeso punto : posiblesCaminos) {
+			if (!visitados.contains(punto)) {
+				//TODO: seleccionar el que tenga menor totalPesoDistancia.
+				//      de existir dos, selecionar el que tenga menor seleccionar el de menor peso.
+				if (punto.getX() != b.getX() && punto.getY() != b.getY()) {
+					visitados.add(punto);
+					menor = buscarCamino(punto, b, visitados);
+					menor.add(punto);
+					listaPuntos = calcularMenor(menor, listaPuntos);
+
+				} else {
+					listaPuntos.add(b);
+					return listaPuntos;
+				}
+			}
+		}
+
+		return listaPuntos;
+	}
+
+	private List<Punto> listaPPtoPunto(List<PuntoPeso> puntoPesoList) {
+		List<Punto> puntoList = new ArrayList<Punto>();
+
+		for (PuntoPeso puntoPeso : puntoPesoList) {
+			puntoList.add(PuntoPesoToPunto(puntoPeso));
+		}
+		return puntoList;
+	}
+
+	private Punto PuntoPesoToPunto(PuntoPeso puntoPeso) {
+
+		Punto punto = new Punto(puntoPeso.getX(), puntoPeso.getY());
+		return punto;
+	}
+
+	private PuntoPeso puntoToPuntoPeso(Punto punto, int peso) {
+		PuntoPeso puntoPeso = new PuntoPeso(punto, mapa.getDensidad(punto.x, punto.y) + 1, peso, 0, null);
+		return puntoPeso;
+	}
+
+	private List<PuntoPeso> calcularMenor(List<PuntoPeso> listaA, List<PuntoPeso> listaB) {
+
+		int a = 0;
+		int b = 0;
+
+		if (!listaB.isEmpty()) {
+
+			for (PuntoPeso punto : listaA) {
+				a += (punto.getPesoCalculado());
+			}
+
+			for (PuntoPeso punto : listaB) {
+				b += (punto.getPesoCalculado());
+			}
+			if (a > b) {
+				return listaB;
+			}
+		}
+		return listaA;
+	}
+
+	private List<PuntoPeso> buscarPuntos(PuntoPeso a, PuntoPeso b) {
+
+		List<PuntoPeso> listaPuntos = new ArrayList<PuntoPeso>();
+		PuntoPeso auxiliar = a;
+
+		auxiliar.setY(auxiliar.getYmenos());
+		auxiliar.setPeso(mapa.getDensidad(auxiliar.getX(), auxiliar.getY()));
+
+		if (auxiliar.getY() > 0 && auxiliar.getX() > 0 && auxiliar.getX() < mapa.LARGO
+				&& ((auxiliar.getY()) < mapa.ALTO)) {
+			if (auxiliar.getPeso() != mapa.MAX_DENSIDAD) {
+				auxiliar.setPesoAcumulado(a.getPesoAcumulado() + 10 * auxiliar.getPeso());
+				auxiliar.setDistanciaAlDestino(calcularDistanciaAlDestino(PuntoPesoToPunto(auxiliar),PuntoPesoToPunto(b)));
+				listaPuntos.add(auxiliar);
+			}
+		}
+
+		auxiliar.setX(auxiliar.getXmenos());
+		auxiliar.setPeso(mapa.getDensidad(auxiliar.getX(), auxiliar.getY()));
+
+		if (auxiliar.getY() > 0 && auxiliar.getX() > 0 && auxiliar.getX() < mapa.LARGO
+				&& ((auxiliar.getY()) < mapa.ALTO)) {
+			if (auxiliar.getPeso() != mapa.MAX_DENSIDAD) {
+				auxiliar.setPesoAcumulado(a.getPesoAcumulado() + 14 * auxiliar.getPeso());
+				auxiliar.setDistanciaAlDestino(calcularDistanciaAlDestino(PuntoPesoToPunto(auxiliar),PuntoPesoToPunto(b)));
+				PuntoPeso auxiliar2 = new PuntoPeso(auxiliar);
+				listaPuntos.add(auxiliar2);
+			}
+		}
+
+		auxiliar.setY(auxiliar.getYmas());
+		auxiliar.setPeso(mapa.getDensidad(auxiliar.getX(), auxiliar.getY()));
+
+		if (auxiliar.getY() > 0 && auxiliar.getX() > 0 && auxiliar.getX() < mapa.LARGO
+				&& ((auxiliar.getY()) < mapa.ALTO)) {
+			if (auxiliar.getPeso() != mapa.MAX_DENSIDAD) {
+				auxiliar.setPesoAcumulado(a.getPesoAcumulado() + 10 * auxiliar.getPeso());
+				auxiliar.setDistanciaAlDestino(calcularDistanciaAlDestino(PuntoPesoToPunto(auxiliar),PuntoPesoToPunto(b)));
+				PuntoPeso auxiliar3 = new PuntoPeso(auxiliar);
+				listaPuntos.add(auxiliar3);
+			}
+		}
+
+		auxiliar.setY(auxiliar.getYmas());
+		auxiliar.setPeso(mapa.getDensidad(auxiliar.getX(), auxiliar.getY()));
+
+		if (auxiliar.getY() > 0 && auxiliar.getX() > 0 && auxiliar.getX() < mapa.LARGO
+				&& ((auxiliar.getY()) < mapa.ALTO)) {
+			if (auxiliar.getPeso() != mapa.MAX_DENSIDAD) {
+				auxiliar.setPesoAcumulado(a.getPesoAcumulado() + 14 * auxiliar.getPeso());
+				auxiliar.setDistanciaAlDestino(calcularDistanciaAlDestino(PuntoPesoToPunto(auxiliar),PuntoPesoToPunto(b)));
+				PuntoPeso auxiliar4 = new PuntoPeso(auxiliar);
+				listaPuntos.add(auxiliar4);
+			}
+		}
+
+		auxiliar.setX(auxiliar.getXmas());
+		auxiliar.setPeso(mapa.getDensidad(auxiliar.getX(), auxiliar.getY()));
+
+		if (auxiliar.getY() > 0 && auxiliar.getX() > 0 && auxiliar.getX() < mapa.LARGO
+				&& ((auxiliar.getY()) < mapa.ALTO)) {
+			if (auxiliar.getPeso() != mapa.MAX_DENSIDAD) {
+				auxiliar.setPesoAcumulado(a.getPesoAcumulado() + 10 * auxiliar.getPeso());
+				auxiliar.setDistanciaAlDestino(calcularDistanciaAlDestino(PuntoPesoToPunto(auxiliar),PuntoPesoToPunto(b)));
+				PuntoPeso auxiliar5 = new PuntoPeso(auxiliar);
+				listaPuntos.add(auxiliar5);
+			}
+		}
+
+		auxiliar.setX(auxiliar.getXmas());
+		auxiliar.setPeso(mapa.getDensidad(auxiliar.getX(), auxiliar.getY()));
+
+		if (auxiliar.getY() > 0 && auxiliar.getX() > 0 && auxiliar.getX() < mapa.LARGO
+				&& ((auxiliar.getY()) < mapa.ALTO)) {
+			if (auxiliar.getPeso() != mapa.MAX_DENSIDAD) {
+				auxiliar.setPesoAcumulado(a.getPesoAcumulado() + 14 * auxiliar.getPeso());
+				auxiliar.setDistanciaAlDestino(calcularDistanciaAlDestino(PuntoPesoToPunto(auxiliar),PuntoPesoToPunto(b)));
+				PuntoPeso auxiliar6 = new PuntoPeso(auxiliar);
+				listaPuntos.add(auxiliar6);
+			}
+		}
+
+		auxiliar.setY(auxiliar.getYmenos());
+		auxiliar.setPeso(mapa.getDensidad(auxiliar.getX(), auxiliar.getY()));
+
+		if (auxiliar.getY() > 0 && auxiliar.getX() > 0 && auxiliar.getX() < mapa.LARGO
+				&& ((auxiliar.getY()) < mapa.ALTO)) {
+			if (auxiliar.getPeso() != mapa.MAX_DENSIDAD) {
+				auxiliar.setPesoAcumulado(a.getPesoAcumulado() + 10 * auxiliar.getPeso());
+				auxiliar.setDistanciaAlDestino(calcularDistanciaAlDestino(PuntoPesoToPunto(auxiliar),PuntoPesoToPunto(b)));
+				PuntoPeso auxiliar7 = new PuntoPeso(auxiliar);
+				listaPuntos.add(auxiliar7);
+			}
+		}
+
+		auxiliar.setY(auxiliar.getYmenos());
+		auxiliar.setPeso(mapa.getDensidad(auxiliar.getX(), auxiliar.getY()));
+
+		if (auxiliar.getY() > 0 && auxiliar.getX() > 0 && auxiliar.getX() < mapa.LARGO
+				&& ((auxiliar.getY()) < mapa.ALTO)) {
+			if (auxiliar.getPeso() != mapa.MAX_DENSIDAD) {
+				auxiliar.setPesoAcumulado(a.getPesoAcumulado() + 14 * auxiliar.getPeso());
+				auxiliar.setDistanciaAlDestino(calcularDistanciaAlDestino(PuntoPesoToPunto(auxiliar),PuntoPesoToPunto(b)));
+				PuntoPeso auxiliar8 = new PuntoPeso(auxiliar);
+				listaPuntos.add(auxiliar8);
+			}
+		}
+
+		return listaPuntos;
+	}
+
+	private int calcularDistanciaAlDestino(Punto a, Punto b) { //cambiar Punto por PuntoPeso
+		int distancia = 0;
 		if (a.x < b.x) {
-			for(int x = a.x ; x < b.x; x++) {
-				listaPuntos.add(new Punto(x, a.y));	
-				mapa.getDensidad(x, a.y);
+			for (int x = a.x; x < b.x; x++) {
+				distancia++;
 			}
 		} else {
-			for(int x = a.x ; x > b.x; x--) {
-				listaPuntos.add(new Punto(x, a.y));
+			for (int x = a.x; x > b.x; x--) {
+				distancia++;
 			}
 		}
 		if (a.y < b.y) {
-			for(int y = a.y ; y < b.y; y++) {
-				listaPuntos.add(new Punto(b.x, y));
+			for (int y = a.y; y < b.y; y++) {
+				distancia++;
 			}
 		} else {
-			for(int y = a.y ; y > b.y; y--) {
-				listaPuntos.add(new Punto(b.x, y));
+			for (int y = a.y; y > b.y; y--) {
+				distancia++;
 			}
 		}
-		cmc.dibujarCamino(listaPuntos);
-		return listaPuntos;
+		return distancia;
 	}
-	
-	private List<Punto> expandirCaminoIdeal(Punto a, Punto b){
-		List<Punto> listaPuntos = new ArrayList<Punto>();
-		
-	}
+
 }
--- src/cmc/PuntoPeso.java
+++ src/cmc/PuntoPeso.java
@@ -3,31 +3,113 @@
  */
 package cmc;
 
+import graficos.Punto;
+
 /**
  * @author alu603
  *
  */
-public class PuntoPeso {
+public class PuntoPeso implements Comparable <PuntoPeso>{
 
-	int x;
-	int y;
-	int peso;
-	int pesoAcumulado;
-	int distanciaAlDestino;
-	int totalPesoDistancia;
-	PuntoPeso predecesor;
-	
-	public PuntoPeso(int x, int y, int peso, int pesoAcumulado, int distanciaAlDestino) {
+	private Punto punto;
+	private int peso;
+	private int pesoAcumulado;
+	private int distanciaAlDestino;
+	private int totalPesoDistancia;
+	private PuntoPeso predecesor;
+
+	public PuntoPeso(Punto punto, int peso, int pesoAcumulado, int distanciaAlDestino, PuntoPeso predecesor) {
 		super();
-		this.x = x;
-		this.y = y;
+		this.punto = punto;
 		this.peso = peso;
 		this.pesoAcumulado = pesoAcumulado;
 		this.distanciaAlDestino = distanciaAlDestino;
 		this.totalPesoDistancia = pesoAcumulado + distanciaAlDestino;
+		this.predecesor = predecesor;
+	}
+
+	public PuntoPeso() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PuntoPeso(PuntoPeso aux) {
+		super();
+		this.punto = new Punto((int)aux.getPunto().getX(), (int)aux.getPunto().getY());
+		this.peso = aux.getPeso();
+		this.pesoAcumulado = aux.getPesoAcumulado();
+		this.distanciaAlDestino = aux.getDistanciaAlDestino();
+		this.totalPesoDistancia = aux.getPesoAcumulado() + aux.getDistanciaAlDestino();
+		this.predecesor = aux.getPredecesor();
 	}
 	
+	public int getX() {
+		return this.punto.x;
+	}
+
+	public int getXmenos() {
+		return this.punto.x - 1;
+	}
+
+	public int getXmas() {
+		return this.punto.x + 1;
+	}
+
+	public int getY() {
+		return this.punto.y;
+	}
+
+	public int getYmenos() {
+		return this.punto.y - 1;
+	}
+
+	public int getYmas() {
+		return this.punto.y + 1;
+	}
+
+	public void setX(int x) {
+		this.punto.x = x;
+	}
+
+	public void setY(int y) {
+		this.punto.y = y;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
 	
+	public int getPesoCalculado() {
+		return peso+1;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public int getPesoAcumulado() {
+		return pesoAcumulado;
+	}
+
+	public void setPesoAcumulado(int pesoAcumulado) {
+		this.pesoAcumulado = pesoAcumulado;
+	}
+
+	public int getDistanciaAlDestino() {
+		return distanciaAlDestino;
+	}
+
+	public void setDistanciaAlDestino(int distanciaAlDestino) {
+		this.distanciaAlDestino = distanciaAlDestino;
+	}
+
+	public int getTotalPesoDistancia() {
+		return totalPesoDistancia;
+	}
+
+	public void setTotalPesoDistancia(int totalPesoDistancia) {
+		this.totalPesoDistancia = totalPesoDistancia;
+	}
+
 	public PuntoPeso getPredecesor() {
 		return predecesor;
 	}
@@ -35,41 +117,19 @@ public class PuntoPeso {
 	public void setPredecesor(PuntoPeso predecesor) {
 		this.predecesor = predecesor;
 	}
-	public int getTotalPesoDistancia() {
-		return totalPesoDistancia;
+
+	public Punto getPunto() {
+		return punto;
 	}
-	public void setTotalPesoDistancia(int totalPesoDistancia) {
-		this.totalPesoDistancia = totalPesoDistancia;
+
+	public void setPunto(Punto punto) {
+		this.punto = punto;
 	}
-	public int getX() {
-		return x;
+
+	@Override
+	public int compareTo(PuntoPeso arg0) {
+		
+		return 0;
 	}
-	public void setX(int x) {
-		this.x = x;
-	}
-	public int getY() {
-		return y;
-	}
-	public void setY(int y) {
-		this.y = y;
-	}
-	public int getPeso() {
-		return peso;
-	}
-	public void setPeso(int peso) {
-		this.peso = peso;
-	}
-	public int getPesoAcumulado() {
-		return pesoAcumulado;
-	}
-	public void setPesoAcumulado(int pesoAcumulado) {
-		this.pesoAcumulado = pesoAcumulado;
-	}
-	public int getDistanciaAlDestino() {
-		return distanciaAlDestino;
-	}
-	public void setDistanciaAlDestino(int distanciaAlDestino) {
-		this.distanciaAlDestino = distanciaAlDestino;
-	}
-	
+
 }
